c66de1ca53762d9e003e66d3a8491f5e171b7041
Mac <mac@192.168.0.111>
2022-06-05 18:42:47 +0700

    add log to fraud controller

--- amigosservices/fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ amigosservices/fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -1,6 +1,7 @@
 package com.amigoscode.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
     private final FraudCheckService fraudCheckService;
 
@@ -24,6 +26,7 @@ public class FraudController {
             @PathVariable("customerId") Integer customerID
     ) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("Fraud check request for customer {}", customerID);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
