82f2c72d977840daf0fdb8d1e2d7ead27e0e7076
Mac <mac@192.168.0.111>
2022-06-05 15:48:16 +0700

    add comment about constructor and AllArgsConstructor Annotation

--- amigosservices/fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ amigosservices/fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -1,14 +1,22 @@
 package com.amigoscode.fraud;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
 public class FraudController {
-
     private final FraudCheckService fraudCheckService;
+
+    /* Constructor if without AllArgsConstructor Annotation
+    public FraudController(FraudCheckService fraudCheckService) {
+        this.fraudCheckService = fraudCheckService;
+    }
+    */
 
     // GET method with one request parameter
     @GetMapping(path = "{customerId}")
