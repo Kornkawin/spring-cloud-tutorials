9c36d9faf42bc164571a7652dc4cd629128c7afa
Mac <mac@192.168.0.111>
2022-06-05 18:44:28 +0700

    add microservice communication via HTTP (REST API)

--- amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,11 +2,14 @@ package com.amigoscode.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         // use builder pattern
         Customer customer = Customer.builder()
@@ -16,8 +19,18 @@ public class CustomerService {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: check if fraudster (request to fraud service)
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer); // MUST flush to get customerId
+
+        // todo: check if fraudster (request GET to fraud service)
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+
         // todo: send notification
     }
 }
